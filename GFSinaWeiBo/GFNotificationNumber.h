5d5e9d4f63d8957db34ff3b20b933735edb9d216
goofyy <1163191060@qq.com>
2015-12-20 15:28:29 +0800

    请求通知数据

---
+++ GFSinaWeiBo/GFNotificationNumber.h
@@ -0,0 +1,13 @@
+//
+//  GFNotificationNumber.h
+//  GFSinaWeiBo
+//
+//  Created by goofygao on 15/12/20.
+//  Copyright © 2015年 goofyy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GFNotificationNumber : NSObject
+
+@end
